d279cacbafc4977b8060f6388bb6cccbb7d7a786
RedCrafter07 <github@redcrafter07.de>
2022-08-12 14:35:13 +0200

    Add transitions & demo routes

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -1,16 +1,34 @@
 import { Routes, Route, useLocation } from 'react-router-dom';
 import Home from './routes/home';
+import { AnimatePresence, motion } from 'framer-motion';
+import Test from './routes/test';
+import MantineDemo from './routes/mantineDemo';
 
 export default function Router() {
 	const location = useLocation();
 
 	return (
 		<>
-			<Routes location={location}>
-				<Route path='/'>
-					<Route index element={<Home />} />
-				</Route>
-			</Routes>
+			<AnimatePresence initial={false} exitBeforeEnter>
+				<motion.div
+					initial={{ opacity: 0 }}
+					animate={{ opacity: 1 }}
+					exit={{ opacity: 0 }}
+					transition={{
+						duration: 0.3,
+						ease: 'easeInOut',
+					}}
+					key={location.pathname}
+				>
+					<Routes location={location}>
+						<Route path='/'>
+							<Route index element={<Home />} />
+							<Route path='/test' element={<Test />} />
+							<Route path='/mantine-demo' element={<MantineDemo />} />
+						</Route>
+					</Routes>
+				</motion.div>
+			</AnimatePresence>
 		</>
 	);
 }
