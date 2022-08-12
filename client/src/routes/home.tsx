f4a1caabe509b795614bfd476b51234a7f836ed9
RedCrafter07 <github@redcrafter07.de>
2022-08-12 14:36:44 +0200

    Make home route a small demo

--- client/src/routes/home.tsx
+++ client/src/routes/home.tsx
@@ -1,9 +1,25 @@
+import { Divider } from '@mantine/core';
+import Base from '../components/Base';
+import Link from '../components/Link';
+
 export default function Home() {
 	return (
-		<div className='bg-zinc-800 text-white min-h-screen p-2'>
-			<div className='mx-auto container'>
-				<p>Hi.</p>
-			</div>
-		</div>
+		<Base>
+			<>
+				<h1>Retmod Template</h1>
+				<h3 className='opacity-50'>with Daisyui, Mantine and SCSS</h3>
+				<Divider className='my-4' />
+				<p>This is a Retmod Project!</p>
+				<br />
+				<p className='btn btn-wide btn-info btn-outline'>
+					This is a DaisyUI button!
+				</p>
+				<br />
+				<br />
+				<Link href='/test' className='btn btn-wide btn-outline btn-accent'>
+					This is a page transition button
+				</Link>
+			</>
+		</Base>
 	);
 }
