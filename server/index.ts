ce468b6d547ad79d21b28011628db1009ba387e2
RedCrafter07 <github@redcrafter07.de>
2022-08-12 14:32:50 +0200

    Support multiple routes

--- server/index.ts
+++ server/index.ts
@@ -34,7 +34,7 @@ app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
+app.get('/*', (req, res) => {
 	res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
